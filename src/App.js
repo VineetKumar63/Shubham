8c179690e47344a653aa081860110c2ea9858492
vineet <vvineet.kumar21@gmail.com>
2024-06-13 09:07:39 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
 import Sidebar from './pages/sidebar';
-import Enquiry from './pages/enquiry';
+import Enquiry from './pages/enquiry/enquiry';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Notification from './pages/notification';
 import Dashboard from './pages/dashboard';
 import Consultations from './pages/Consultations';
-import EnquiryDashboard from './pages/NewEnquiryDashboard';
+import EnquiryDashboard from './pages/enquiry/NewEnquiryDashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes >
-        <Route path="/"
-            element={
-              <Sidebar/>} >
-              <Route path='/enquiry' element={<Enquiry/>} />
-              <Route path='/notification' element={<Notification/>} />
-              <Route path='/' element={<EnquiryDashboard/>} />
-              <Route path='/consultations' element={<Consultations/>} />
+        <Route path="/" element={ <Sidebar/>} >
+          <Route path='/enquiryForm' element={<Enquiry/>} />
+          <Route path='/notification' element={<Notification/>} />
+          <Route path='/enquiry' element={<EnquiryDashboard/>} />
+          <Route path='/consultations' element={<Consultations/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/sidebar.jsx
+++ src/pages/sidebar.jsx
@@ -7,46 +7,48 @@ function Sidebar() {
   return (
     <div className="container-fluid">
       <div className="d-flex">
-        <div className="sidebar ">
-        <h3 className="logo-text">ENQUIRY</h3>
+        <div className="sidebar">
+        <div className="logo py-4">
+        <h3 className="logo-text"><img src="/logo.png" alt=""  />Enquiries</h3>
+        </div>
           <ul>
-          <li>
-          <Link to="/notification" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline ">Notification</span>
+          <li >         
+            <Link to="/notification" className="nav-link px-1 align-middle">
+                <span className="ms-1 d-none d-sm-inline "><img src="/notifications_active.svg " alt=""/>Notification</span>
               </Link>
           </li>
             <li><Link to="/" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Dashboard</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/dashboard.svg " alt=""/>Dashboard</span>
               </Link></li>
             <li>
               <Link to="/enquiry" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Enquiry</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/list_alt.svg " alt=""/>Enquiry</span>
               </Link>
             </li>
             <li><Link to="/consultations" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Consultations</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/groups.svg " alt=""/>Consultations</span>
               </Link>
             </li>
             <li><Link to="/orders" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Orders</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/local_florist.svg " alt=""/>Orders</span>
               </Link></li>
             <li><Link to="/pricing" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Pricing</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/local_offer.svg " alt=""/>Pricing</span>
               </Link></li>
             <li><Link to="/proposal" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Proposal</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/notes.svg " alt=""/>Proposal</span>
               </Link></li>
             <li><Link to="/wholesale" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Wholesale</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/spa.svg " alt=""/>Wholesale</span>
               </Link></li>
             <li><Link to="/production" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Production</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/local_florist (2).svg " alt=""/>Production</span>
               </Link></li>
             <li><Link to="/delivery" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Delivery</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/airport_shuttle.svg " alt=""/>Delivery</span>
               </Link></li>
             <li><Link to="/libraries" className="nav-link px-1 align-middle">
-                <span className="ms-1 d-none d-sm-inline">Libraries</span>
+                <span className="ms-1 d-none d-sm-inline"><img src="/book-open (2).svg " alt=""/>Libraries</span>
               </Link></li>
           </ul>
         </div>
